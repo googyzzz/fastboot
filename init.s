1c355a6770e8eecce6eaf64b0e5418fafdc5311a
googyzzz <googy@web.de>
2015-02-12 22:09:47 +0100

    GPIO
    Das sollte Pin PU0 direkt high setzen, im Wiki steht wo sich dieser Pin befindet

--- init.s
+++ init.s
@@ -30,6 +30,24 @@ _start:
    msr   cpsr_c,   r2
    ldr   sp,           =_estack
    
+   /* set PU0 function to GPIO */
+   mov   r0,   #0x1
+   ldr   r1,   =0x6000D500
+   str   r0,   [r1]
+   
+   /* set PU0 direction to output */
+   ldr   r1,   =0x6000D510
+   str   r0,   [r1]
+   
+   /* set PU0 value to high */
+   ldr   r1,   =0x6000D520
+   str   r0,   [r1]
+   
+   /* activate PU0 output stage */
+   mov   r0,   #0x20
+   ldr   r1,   =0x70003184
+   str   r0,   [r1]
+   
    bl main
    b .
 
